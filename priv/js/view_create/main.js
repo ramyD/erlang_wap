75f4b7f3d473fb2d1f9b34912746e06a2a2c73ab
ramyD <ramy.daghstani@gmail.com>
2012-10-30 02:31:12 -0400

    added bootstrap theme to one of the views until i get templating working

--- priv/js/view_create/main.js
+++ priv/js/view_create/main.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-	jQuery.getJSON("/create/get_template_names", function(data) {
+	"use strict";
+	$.getJSON("/create/get_template_names", function(data) {
 		$("#templates").kendoDropDownList({
 			dataTextField: "text",
 			dataValueField: "value",
-			dataSource: data
+			dataSource: data,
+			change: constructForm
 		});
 	});
+
+	constructForm();
 });
+
+// shows the template according to what was picked through the dropdown list
+function constructForm() {
+	"use strict";
+	var value = $('#templates').val();
+	console.log("template is: " + value);
+}
